16aafb0193fa999b1126be586124f53154c8050f
Ravi Kant Sahu <ravirks444777@gmail.com>
2024-09-05 22:33:26 +0530

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { DepartmentComponent } from './CMMS/department/department.component';
 
 export const routes: Routes = [
     { path: 'cmms-department', component: DepartmentComponent },
+    { path: 'cmms-location', component: DepartmentComponent },
+    { path: 'cmms-group', component: DepartmentComponent },
+    { path: 'cmms-asset', component: DepartmentComponent },
     // { path: 'one', component: ComponentOneComponent },
     // { path: 'two', component: ComponentTwoComponent },
     // { path: 'three', component: ComponentThreeComponent },
